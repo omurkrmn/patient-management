a45863e5f7ac6d4d1d948e2635772253d178b6be
omurkrmn <ahmetomurkaraman06@gmail.com>
2026-02-03 20:10:37 +0300

    some update for service and dto

---
+++ patient-service/src/main/java/com/omurkrmn/patientservice/dto/PatientResponseDTO.java
@@ -0,0 +1,12 @@
+package com.omurkrmn.patientservice.dto;
+
+
+public class PatientResponseDTO {
+
+    private String id;
+    private String name;
+    private String email;
+    private String address;
+    private String dateOfBirth;
+
+}
--- patient-service/src/main/java/com/omurkrmn/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/omurkrmn/patientservice/service/PatientService.java
@@ -1,7 +1,10 @@
 package com.omurkrmn.patientservice.service;
 
+import com.omurkrmn.patientservice.model.Patient;
 import com.omurkrmn.patientservice.repository.PatientRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PatientService {
@@ -10,5 +13,9 @@ public class PatientService {
 
     public PatientService(PatientRepository patientRepository) {
         this.patientRepository = patientRepository;
+    }
+
+    public List<PatientResponseDTO> getPatients () {
+        List<Patient> patients = patientRepository.findAll();
     }
 }
